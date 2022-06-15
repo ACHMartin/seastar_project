7485631637c1963aaf125b59ee56289dfd3c574e
clive3 <cneil@noc.ac.uk>
2022-06-15 13:19:56 +0100

    Update clives_functions.py

--- processing/clives_functions.py
+++ processing/clives_functions.py
@@ -1,21 +1,11 @@
-def doSomething(type_of_tree_str=None):
-    """This function checks the input to see whether it is a recognised tree type,
-    it prints to console whether or not it is recognised and returns a suitabel Boolean
-
-    :param type_of_tree_str a string containing the tree type to be tested, defaults to  None
-    :type String
-
-    :return: whther or not the input parameters is in th elist of know trees
-    :rtype: Boolean
+def doSomething():
     """
-    KNOWN_TREES = ['ash', 'elm', 'oak', 'rowan', 'beech']
-    is_known = False
-    if type_of_tree_str.lower() in KNOWN_TREES:
+    this is a dummy function that returns True
+        Returns
+    -------
+    True
+    """
 
-        print(f'I recognise this tree: {type_of_tree_str}')
-        is_known = True
-    else:
+    print("Clive's function within the processing package is doing something.")
 
-        print(f'I do not recognise this tree: {type_of_tree_str}')
-
-    return is_known
+    return True
