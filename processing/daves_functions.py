8c90ec9fa3e23b9c12df6d61a1b69e5eca1527b0
DavidMcCann-NOC <107252303+DavidMcCann-NOC@users.noreply.github.com>
2022-06-15 08:52:01 +0100

    Update daves_functions.py

--- processing/daves_functions.py
+++ processing/daves_functions.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 14 15:40:24 2022
 
-@author: davidmccann
-"""
+def plotSimpleLine():
+    import matplotlib.pyplot as plt
+    import numpy as np
+    x = np.linspace(-5,5,100)
+    y = 2*x+1
+    plt.plot(x, y, '-r', label='y=2x+1')
+    plt.title('Graph of y=2x+1')
+    plt.xlabel('x', color='#1C2833')
+    plt.ylabel('y', color='#1C2833')
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.show()
 
-def printPoem():
-    
-    print('os hoffech wybod..')
