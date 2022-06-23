a7d7447ba98ddfd66643fbae83155bfcf2e67836
clive3 <cneil@noc.ac.uk>
2022-06-23 19:07:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='seastar',
     version='1.0',
-    packages=find_packages(include=['utils', 'examples', 'gmfs', 'retrieval'])
+    packages=find_packages(include=['utils', 'examples', 'gmfs', 'retrieval']),
+    setup_requires=['numpy', 'xarray'],
 )
