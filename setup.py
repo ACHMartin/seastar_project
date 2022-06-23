bfddeb8945a60f0d200011e1d2a59d8d16e52c5b
NOC-SEASTAR <107064894+NOC-SEASTAR@users.noreply.github.com>
2022-06-23 20:26:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 from setuptools import setup, find_packages
-from setuptools.command.build_ext import build_ext as _build_ext
 
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
         
 setup(
     name='seastar',
     version='1.0',
     packages=find_packages(include=['utils', 'examples', 'gmfs', 'retrieval']),
-    cmdclass={'build_ext':build_ext},
-    setup_requires=['numpy'],
+    setup_requires=['numpy'],['xrray']
 )
