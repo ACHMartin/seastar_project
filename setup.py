741c5a77dc14988b6a4f1c62a40382656c18aa42
NOC-SEASTAR <107064894+NOC-SEASTAR@users.noreply.github.com>
2022-06-23 17:56:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='seastar',
     version='1.0',
-    packages=find_packages(include=['seastar', 'seastar.utils'])
+    packages=find_packages()
 )
