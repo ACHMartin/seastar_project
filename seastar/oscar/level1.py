3dc22e370f45c38316eff58987a386be0c8ccb4f
DavidMcCann-NOC <107252303+DavidMcCann-NOC@users.noreply.github.com>
2022-10-07 10:15:40 +0100

    Update level1.py
    Added level1 combined look-direction intitialisation and a function to convert Metasensing's Tx/Rx polarization format into a standard 'HH / VV' form.

--- seastar/oscar/level1.py
+++ seastar/oscar/level1.py
@@ -6,7 +6,7 @@ Created on Fri Sep 16 13:48:48 2022
 
 @author: admartin, dlmccann
 """
-
+import re
 import numpy as np
 import xarray as xr
 import scipy as sp
@@ -14,6 +14,25 @@ import seastar
 
 
 def init_level1_dataset(dsf, dsa):
+    """
+    Initialise level1 dataset.
+
+    Initialise a combined-look-direction level1 OSCAR dataset from separate
+    look directions and add a corresponding 'Antenna' dim and coord.
+
+    Parameters
+    ----------
+    dsf : xarray.Dataset
+        OSCAR dataset in the fore-beam look direction
+    dsa : xarray.Dataset
+        OSCAR dataset in the aft-beam look direction
+
+    Returns
+    -------
+    ds_level1 : xarray.Dataset
+        OSCAR dataset with combined look directions
+
+    """
     ds_level1 = xr.concat([dsf,
                            dsa],
                           'Antenna', join='outer',
@@ -21,6 +40,31 @@ def init_level1_dataset(dsf, dsa):
     ds_level1 = ds_level1.assign_coords(Antenna=('Antenna', ['Fore', 'Aft']))
     return ds_level1
 
+
+def check_antenna_polarization(ds):
+    """
+    Check polarization and standardize format.
+
+    Check the Tx and Rx polarization variables in an OSCAR dataset and
+    create a new 'Polarization' variable in a standard 'HH', 'VV' style
+    format.
+
+    Parameters
+    ----------
+    ds : xarray.Dataset
+        OSCAR SAR dataset
+
+    Returns
+    -------
+    ds : xarray.Dataset
+        OSCAR SAR dataset
+
+    """
+    polarization = [str(ds.TxPolarization.data), str(ds.RxPolarization.data)]
+    polarization = ''.join(polarization)
+    ds['Polarization'] = polarization = re.sub('[^VH]', '', polarization)
+
+    return ds
 
 
 def add_antenna_baseline(ds, baseline):
@@ -312,11 +356,6 @@ def init_level2(dsf, dsa):
     return level2
 
 
-
-
-
-
-
 def init_auxiliary(level2, dsa, dsf, u10, wind_direction):
     
     "A Dataset containing WindSpeed, WindDirection,"
@@ -331,24 +370,24 @@ def init_auxiliary(level2, dsa, dsf, u10, wind_direction):
     aux = xr.Dataset()
     aux['WindSpeed'] = WindSpeed
     aux['WindDirection'] = WindDirection
-    aux['LookDirection'] = xr.concat([dsf.AntennaAzimuthImage,
-                                      dsa.AntennaAzimuthImage],
-                                     'Antenna', join='outer')
-    aux['LookDirection'] = aux.LookDirection.assign_coords(
-        Antenna=('Antenna', ['Fore', 'Aft']))
-    aux['IncidenceAngleImage'] = xr.concat([dsf.IncidenceAngleImage,
-                                            dsa.IncidenceAngleImage],
-                                           'Antenna', join='outer')
-    aux['IncidenceAngleImage'] = aux.IncidenceAngleImage.assign_coords(
-        Antenna=('Antenna', ['Fore', 'Aft']))
-    #Polarization (1=VV; 2=HH)
-    aux['Polarization'] = xr.DataArray(data=[np.zeros(WindSpeed.shape)+1,
-                                       np.zeros(WindSpeed.shape)+1],
-                                       dims=aux.LookDirection.dims,
-                                       coords=aux.LookDirection.coords,
-                                       )
-    aux['CentralWavenumber'] = dsf.CentralWavenumber.data
-    aux['CentralFreq'] = dsf.CentralFreq.data
+#    aux['LookDirection'] = xr.concat([dsf.AntennaAzimuthImage,
+#                                      dsa.AntennaAzimuthImage],
+#                                     'Antenna', join='outer')
+#    aux['LookDirection'] = aux.LookDirection.assign_coords(
+#        Antenna=('Antenna', ['Fore', 'Aft']))
+#    aux['IncidenceAngleImage'] = xr.concat([dsf.IncidenceAngleImage,
+#                                            dsa.IncidenceAngleImage],
+#                                           'Antenna', join='outer')
+#    aux['IncidenceAngleImage'] = aux.IncidenceAngleImage.assign_coords(
+#        Antenna=('Antenna', ['Fore', 'Aft']))
+#    #Polarization (1=VV; 2=HH)
+#    aux['Polarization'] = xr.DataArray(data=[np.zeros(WindSpeed.shape)+1,
+#                                       np.zeros(WindSpeed.shape)+1],
+#                                       dims=aux.LookDirection.dims,
+#                                       coords=aux.LookDirection.coords,
+#                                       )
+#    aux['CentralWavenumber'] = dsf.CentralWavenumber.data
+#    aux['CentralFreq'] = dsf.CentralFreq.data
     
     return aux
     
