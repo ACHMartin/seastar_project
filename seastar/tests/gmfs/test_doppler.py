699908471218ae1cab533b93d7d974d9c30b38f8
ACHMartin <admartin@noc.ac.uk>
2023-02-07 16:33:22 +0100

    remove the loop along antenna on doppler/compute_wasv (back before the 13th of December) + one dataset test in test_doppler/test_compute_wasv

--- seastar/tests/gmfs/test_doppler.py
+++ seastar/tests/gmfs/test_doppler.py
@@ -3,6 +3,7 @@
 import numpy as np
 import xarray as xr
 import pytest
+from seastar.utils.tools import dotdict
 
 from seastar.gmfs import doppler
 
@@ -22,16 +23,78 @@ def point_inci_angle():
 def point_polarization():
     return 'VV' # in degree
 
+@pytest.fixture
+def level1_geo_dataset():
+    level1 = xr.Dataset(
+        data_vars=dict(
+            CentralWavenumber=([], 270),
+            CentralFreq=([], 13.5 * 10 ** 9),
+            IncidenceAngleImage=(['across', 'along', 'Antenna'], np.full([5, 6, 4], 30)),
+            AntennaAzimuthImage=(['across', 'along', 'Antenna'],
+                                 np.stack((np.full([5, 6], 45),
+                                           np.full([5, 6], 90),
+                                           np.full([5, 6], 90),
+                                           np.full([5, 6], 135)
+                                           ), axis=-1
+                                          )
+                                 ),
+            Polarization=(['across', 'along', 'Antenna'],
+                          np.stack((np.full([5, 6], 'VV'),
+                                    np.full([5, 6], 'VV'),
+                                    np.full([5, 6], 'HH'),
+                                    np.full([5, 6], 'VV')
+                                    ), axis=-1
+                                   )
+                          ),
+            #             Sigma0=( ['across','along','Antenna'], np.full([5, 6,4], 1.01) ),
+            #             dsig0=( ['across','along','Antenna'], np.full([5, 6,4], 0.05) ),
+            #             RVL=( ['across','along','Antenna'], np.full([5, 6,4], 0.5) ),
+            #             drvl=( ['across','along','Antenna'], np.full([5, 6,4], 0.01) ),
+        ),
+        coords=dict(
+            across=np.arange(0, 5),
+            along=np.arange(0, 6),
+            Antenna=['Fore', 'MidV', 'MidH', 'Aft'],
+        ),
+    )
+
+    level1 = level1.set_coords([
+        'CentralWavenumber',
+        'CentralFreq',
+        'IncidenceAngleImage',
+        'AntennaAzimuthImage',
+        'Polarization',
+    ])
+
+    geo = xr.Dataset(
+        data_vars=dict(
+            WindSpeed=(['across', 'along'], np.full([5, 6], 10)),
+            WindDirection=(['across', 'along'], np.full([5, 6], 150)),
+            CurrentVelocity=(['across', 'along'], np.full([5, 6], 1)),
+            CurrentDirection=(['across', 'along'], np.full([5, 6], 150)),
+        ),
+        coords=dict(
+            across=np.arange(0, 5),
+            along=np.arange(0, 6),
+        ),
+    )
+
+
+    return dict({'level1': level1, 'geo': geo})
+
+@pytest.fixture
+def gmf_mouche():
+    gmf = dotdict({'nrcs': dotdict({'name': 'nscat4ds'})})
+    gmf['doppler'] = dotdict({'name': 'mouche12'})
+    return gmf
+
 # TODO might be ideal to have a table of input and expected output
 
 
+def test_mouche12(point_wind_speed, point_relative_dir, point_inci_angle, point_polarization):
 
-def test_mouche12():
-    # print(type(str(point_polarization)))
-    # print(str(point_polarization))
-    # print(type(point_inci_angle))
-    # assert doppler.mouche12(point_wind_speed, point_relative_dir, point_inci_angle, point_polarization) \
-    #        == pytest.approx(1, 50) # 1 +- 5 => error on pol in mouche12, with pol expected as a str not a function
+    assert doppler.mouche12(point_wind_speed, point_relative_dir, point_inci_angle, point_polarization) \
+           == pytest.approx(24, 1) # 1 +- 5 => error on pol in mouche12, with pol expected as a str not a function
     # Test points
     assert doppler.mouche12(7, 3, 30, 'VV') \
             == pytest.approx(24, 1)  # in Hz 24 +- 1
@@ -78,6 +141,25 @@ def test_mouche12():
         rtol=1
     )
 
+def test_compute_wasv(level1_geo_dataset, gmf_mouche):
+    level1 = level1_geo_dataset['level1']
+    L1ant = level1.sel(Antenna='Fore')
+    geo = level1_geo_dataset['geo']
+
+    # Test dataset with .sel(Antenna)
+    xr.testing.assert_allclose(
+        doppler.compute_wasv(
+            L1ant,
+            geo,
+            gmf=gmf_mouche.doppler.name
+        ).drop_vars(['CentralWavenumber', 'CentralFreq', 'IncidenceAngleImage', 'AntennaAzimuthImage',
+                     'Polarization','Antenna','along','across']),
+        xr.DataArray(
+            data=np.full( L1ant.AntennaAzimuthImage.shape, 0.32),
+            dims=( L1ant.dims)
+        ),
+        rtol=0.01
+    )
 
 
 
