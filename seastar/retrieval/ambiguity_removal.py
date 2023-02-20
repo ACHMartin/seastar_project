b49c5c035b80a4b869e5d505381f49312bf23d73
ACHMartin <admartin@noc.ac.uk>
2023-02-20 15:00:35 +0100

    - coding of "ambiguity_closest_to_truth" + doc - change behavior of ambiguity_sort_by_cost with returning index of dataArray + doc

--- seastar/retrieval/ambiguity_removal.py
+++ seastar/retrieval/ambiguity_removal.py
@@ -15,39 +15,70 @@ from seastar.utils.tools import dotdict
 import pdb
 
 
-def ambiguity_closest_to_truth(truth, results, method='windcurrent', windcurrentratio=10):
+def ambiguity_closest_to_truth(lmout, truth, windcurrentratio=10):
     """
-
-    METHOD
-    Distance calculated using:
-    - wind and current with a given ratio between wind and current
-    - only wind
-    - only current
-
+    Define a dictionary (windcurrent, wind, current) of indexes for lmout.x on Ambiguities closest to the Truth.
+    Closest distances are defined as euclidian distance on the wind only (wind), current only (current)
+    or combining distance between wind and current using the given windcurrentratio (windcurrent).
     Parameters
     ----------
+    lmout : ``xarray.Dataset``
+        Have to contain the following ".x" data_vars and
+         coordinates "x_variables" = [u,v,c_u,c_v]; "Ambiguities";
     truth : ``xarray.Dataset``
-        Truth in term of:
-        - geophysical parameters: WindSpeed, WindDirection, CurrentVel, CurrentDir, others (waves)
-        - geographic coordinates: longitude, latitude (optional?)
-        ...
-    results : ``xarray.Dataset``
-        unsorted results
-
-
+        Have to contain the following geophysical parameters:
+        WindSpeed, WindDirection, CurrentVelocity, CurrentDirection, others (waves)
+        Should be in the same dimension as lmout
+    windcurrentratio : ``float``
+        ratio to combine the distance between wind and current. Default value of 10
     Returns
     -------
-    level2 : ``xarray.Dataset``
-        L2.shape (antenna, across, along)
+    index : ``dict`` of ``xarray.DataArray``
+        with keys "windcurrent", "wind", "current" corresponding on which quantity the distance is calculated
+        combining dataArray. Can be applied directly to lmout with lmout.isel(Ambiguities=ind['windcurrent'])
     """
 
-    print("To Be Done")
+    mytruth = xr.Dataset()
+    (mytruth['WindU'], mytruth['WindV']) = \
+        seastar.utils.tools.windSpeedDir2UV(truth.WindSpeed, truth.WindDirection)
+    (mytruth['CurrentU'], mytruth['CurrentV']) = \
+        seastar.utils.tools.currentVelDir2UV(truth.CurrentVelocity, truth.CurrentDirection)
+    mytruth['x'] = xr.concat(
+        [mytruth['WindU'], mytruth['WindV'], mytruth['CurrentU'], mytruth['CurrentV']],
+        'x_variables'
+    )
 
-    return level2
+    err = truth - lmout # keep only the 'x' variable
+
+    err['dist_x_reduce'] = xr.concat(
+        [err.x.sel(x_variables='u')**2 + err.x.sel(x_variables='v')**2,
+         windcurrentratio * (err.x.sel(x_variables='c_u')**2 + err.x.sel(x_variables='c_v')**2) ],
+        'x_reduce'
+    )
+    err.coords['x_reduce'] = ['uv','c_uv']
+
+    ind = dict({
+        'windcurrent': err.dist_x_reduce.sum(dim='x_reduce').argmin(dim='Ambiguities'),
+        'wind': err.dist_x_reduce.sel(x_reduce='uv').argmin(dim='Ambiguities'),
+        'current': err.dist_x_reduce.sel(x_reduce='c_uv').argmin(dim='Ambiguities'),
+    })
+
+    return ind
 
 def ambiguity_sort_by_cost(lmout):
-    print("To Be Done - ambiguity_sort_by_cost")
-    return lmout.sortby('cost')
+    """
+    Return an indexes for lmout on Ambiguities for the smallest cost.
+    Parameters
+    ----------
+    lmout : ``xarray.Dataset``
+        Have to contain the following ".cost" data_vars
+    Returns
+    -------
+    index : ``xarray.DataArray``
+        a dataArray index. Can be applied directly to lmout with lmout.isel(Ambiguities=index_cost)
+    """
+    index = lmout.cost.argmin(dim='Ambiguities')
+    return index
 
 
 
@@ -57,7 +88,7 @@ def solve_ambiguity(lmout, ambiguity):
     - cost values: chi2
     - closest to truth
     ...
-
+method='windcurrent',
 
 
     """
