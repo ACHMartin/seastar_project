584bb8cd9cf9c1d03fb7396a915aaa01263d71e4
ACHMartin <admartin@noc.ac.uk>
2023-06-16 18:59:35 +0200

    adapt the unit test test_level2.py to include the change of Wind to EarthRelativeWind or OceanSurfaceWind. I didn't implement specific test to see if "Wind" inputs without "EarthRelative" or "OceanSurface" is rejected or not.
    + correction of RVL => RSV
    
    (the unit test is still failing)

--- seastar/tests/retrieval/test_level2.py
+++ seastar/tests/retrieval/test_level2.py
@@ -34,8 +34,8 @@ def level1_geo_dataset(gmf_mouche):
                           ),
             #             Sigma0=( ['across','along','Antenna'], np.full([5, 6,4], 1.01) ),
             #             dsig0=( ['across','along','Antenna'], np.full([5, 6,4], 0.05) ),
-            #             RVL=( ['across','along','Antenna'], np.full([5, 6,4], 0.5) ),
-            #             drvl=( ['across','along','Antenna'], np.full([5, 6,4], 0.01) ),
+            #             RSV=( ['across','along','Antenna'], np.full([5, 6,4], 0.5) ),
+            #             dRSV=( ['across','along','Antenna'], np.full([5, 6,4], 0.01) ),
         ),
         coords=dict(
             across=np.arange(0, 5),
@@ -54,8 +54,8 @@ def level1_geo_dataset(gmf_mouche):
 
     geo = xr.Dataset(
         data_vars=dict(
-            WindSpeed=(['across', 'along'], np.full([5, 6], 10)),
-            WindDirection=(['across', 'along'], np.full([5, 6], 150)),
+            EarthRelativeWindSpeed=(['across', 'along'], np.full([5, 6], 10)),
+            EarthRelativeWindDirection=(['across', 'along'], np.full([5, 6], 150)),
             CurrentVelocity=(['across', 'along'], np.full([5, 6], 1)),
             CurrentDirection=(['across', 'along'], np.full([5, 6], 150)),
         ),
@@ -65,21 +65,21 @@ def level1_geo_dataset(gmf_mouche):
         ),
     )
 
+    geo = seastar.utils.tools.EarthRelativeSpeedDir2all(geo)
 
+    # geo['u'], geo['v'] = seastar.utils.tools.windSpeedDir2UV(geo.EarthRelativeWindSpeed, geo.EarthRelativeWindDirection)
+    # [geo['c_u'], geo['c_v']] = seastar.utils.tools.currentVelDir2UV(geo.CurrentVelocity, geo.CurrentDirection)
+    # geo['vis_u'] = geo['u'] - geo['c_u']
+    # geo['vis_v'] = geo['v'] - geo['c_v']
+    # geo['vis_wspd'], vis_wdir = \
+    #     seastar.utils.tools.windUV2SpeedDir(
+    #         geo['vis_u'], geo['vis_v']
+    #     )
+    # geo['vis_wdir'] = (geo.vis_wspd.dims, vis_wdir)
 
-    geo['u'], geo['v'] = seastar.utils.tools.windSpeedDir2UV(geo.WindSpeed, geo.WindDirection)
-    [geo['c_u'], geo['c_v']] = seastar.utils.tools.currentVelDir2UV(geo.CurrentVelocity, geo.CurrentDirection)
-    geo['vis_u'] = geo['u'] - geo['c_u']
-    geo['vis_v'] = geo['v'] - geo['c_v']
-    geo['vis_wspd'], vis_wdir = \
-        seastar.utils.tools.windUV2SpeedDir(
-            geo['vis_u'], geo['vis_v']
-        )
-    geo['vis_wdir'] = (geo.vis_wspd.dims, vis_wdir)
-
-    # TODO geo should use the RelativeWind
+    # TODO geo should use the RelativeWind # TODO solved with EarthRelative...2all
     level1['Sigma0'] = seastar.gmfs.nrcs.compute_nrcs(level1, geo, gmf_mouche.nrcs) * 1.001
-    level1['RVL'] = xr.concat([
+    level1['RSV'] = xr.concat([
         seastar.gmfs.doppler.compute_total_surface_motion(
             level1.sel(Antenna=ant),
             geo,
@@ -87,13 +87,13 @@ def level1_geo_dataset(gmf_mouche):
         ) for ant in level1.Antenna.data],
         'Antenna',
         join='outer')
-    # Add NaN for RVL for the mid antennas
-    level1.RVL[1, :, :] = np.full([5, 6], np.nan)
-    level1.RVL[2, :, :] = np.full([5, 6], np.nan)
+    # Add NaN for RSV for the mid antennas
+    level1.RSV[1, :, :] = np.full([5, 6], np.nan)
+    level1.RSV[2, :, :] = np.full([5, 6], np.nan)
     # Noise
     noise = level1.drop_vars([var for var in level1.data_vars])
     noise['Sigma0'] = level1.Sigma0 * 0.05
-    noise['RVL'] = level1.RVL * 0.05
+    noise['RSV'] = level1.RSV * 0.05
 
     # TODO, we shouldn't do this, it should be fixed value, without calling a function
     return dict({'level1': level1, 'geo': geo, 'noise': noise})
@@ -127,12 +127,12 @@ def test_wind_current_retrieval(level1_geo_dataset, gmf_mouche):
             x=(['x_variables'], [-4.50, 7.80, 0.50, -0.86]),
             CurrentU=([], 0.50),
             CurrentV=([], -0.86),
-            WindU=([], -4.50),
-            WindV=([], 7.80),
+            EarthRelativeWindU=([], -4.50),
+            EarthRelativeWindV=([], 7.80),
             CurrentVelocity=([], 1.00),
             CurrentDirection=([], 150.00),
-            WindSpeed=([], 9.00), # TODO should be 10 if AbsoluteWind, 9 if RelativeWind
-            WindDirection=([], 150.00),
+            EarthRelativeWindSpeed=([], 10.00), # TODO should be 10 if AbsoluteWind, 9 if RelativeWind
+            EarthRelativeWindDirection=([], 150.00),
         ),
     )
 
