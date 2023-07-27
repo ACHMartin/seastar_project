Correction of IntensityAVG Master and Slave for fixing coherence calculation

seastar/oscar/level1.py
@@ -310,11 +310,11 @@ def compute_multilooking_Master_Slave(ds, window=3,
     ds_out.Interferogram.attrs['long_name'] = 'Interferogram'
     ds_out.Interferogram.attrs['units'] = 'rad'
 
-    ds_out['IntensityAvgMaster'] = np.abs(
-        (ds.SigmaSLCMaster ** 2) \
+    ds_out['IntensityAvgMaster'] = (np.abs
+        (ds.SigmaSLCMaster) ** 2) \
             .rolling({ds.SigmaSLCMaster.dims[1]: window}).mean() \
             .rolling({ds.SigmaSLCMaster.dims[0]: window}).mean()
-    )
+
     ds_out.IntensityAvgMaster.attrs['long_name'] = \
         'Intensity Master'
     ds_out.Intensity.attrs['description'] = \
@@ -322,11 +322,11 @@ def compute_multilooking_Master_Slave(ds, window=3,
     ds_out.IntensityAvgMaster.attrs['units'] = ''
 
     if 'SigmaSLCSlave' in ds.data_vars:
-        ds_out['IntensityAvgSlave'] = np.abs(
-            (ds.SigmaSLCSlave ** 2)\
+        ds_out['IntensityAvgSlave'] = (np.abs
+            (ds.SigmaSLCSlave) ** 2)\
                 .rolling({ds.SigmaSLCSlave.dims[1]: window}).mean()\
                 .rolling({ds.SigmaSLCSlave.dims[0]: window}).mean()
-        )
+
         ds_out['Coherence'] =\
             ds_out.Intensity / np.sqrt(ds_out.IntensityAvgMaster
                                        * ds_out.IntensityAvgSlave)