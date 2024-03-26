2f0b85392323c64ba319b5103914c4c5df5b19c7
Jakub Michalski <jmichalski3009@gmail.com>
2024-03-26 10:06:57 +0000

    change cost to distance

--- seastar/tests/retrieval/test_spatial_ambiguity_selection.py
+++ seastar/tests/retrieval/test_spatial_ambiguity_selection.py
@@ -13,15 +13,13 @@ def L2_small2D():
     values = np.array([[3, 3, 3], [3, 3, 3], [3, 3, 3]])
     return xr.Dataset(
         data_vars={
-            'CurrentU': (["CrossRange", "GroundRange"], values),
-            'CurrentV': (["CrossRange", "GroundRange"], values),
-            'EarthRelativeWindU': (["CrossRange", "GroundRange"], values),
-            'EarthRelativeWindV': (["CrossRange", "GroundRange"], values)
+            "CurrentU": (["CrossRange", "GroundRange"], values),
+            "CurrentV": (["CrossRange", "GroundRange"], values),
+            "EarthRelativeWindU": (["CrossRange", "GroundRange"], values),
+            "EarthRelativeWindV": (["CrossRange", "GroundRange"], values),
         },
-        coords={
-            'CrossRange': np.arange(3),
-            'GroundRange': np.arange(3)
-        })
+        coords={"CrossRange": np.arange(3), "GroundRange": np.arange(3)},
+    )
 
 
 @pytest.fixture
@@ -30,15 +28,13 @@ def initial():
     values = np.zeros((5, 4))
     return xr.Dataset(
         data_vars={
-            'CurrentU': (["CrossRange", "GroundRange"], values),
-            'CurrentV': (["CrossRange", "GroundRange"], values),
-            'EarthRelativeWindU': (["CrossRange", "GroundRange"], values),
-            'EarthRelativeWindV': (["CrossRange", "GroundRange"], values)
+            "CurrentU": (["CrossRange", "GroundRange"], values),
+            "CurrentV": (["CrossRange", "GroundRange"], values),
+            "EarthRelativeWindU": (["CrossRange", "GroundRange"], values),
+            "EarthRelativeWindV": (["CrossRange", "GroundRange"], values),
         },
-        coords={
-            'CrossRange': np.arange(5),
-            'GroundRange': np.arange(4)
-        })
+        coords={"CrossRange": np.arange(5), "GroundRange": np.arange(4)},
+    )
 
 
 @pytest.fixture
@@ -50,81 +46,97 @@ def lmout():
     values[3, :, :] = np.full((5, 4), 3)
     return xr.Dataset(
         data_vars={
-            'CurrentU': (["Ambiguities", "CrossRange", "GroundRange"], values),
-            'CurrentV': (["Ambiguities", "CrossRange", "GroundRange"], values),
-            'EarthRelativeWindU': (
-                ["Ambiguities", "CrossRange", "GroundRange"], values),
-            'EarthRelativeWindV': (
-                ["Ambiguities", "CrossRange", "GroundRange"], values)
+            "CurrentU": (["Ambiguities", "CrossRange", "GroundRange"], values),
+            "CurrentV": (["Ambiguities", "CrossRange", "GroundRange"], values),
+            "EarthRelativeWindU": (
+                ["Ambiguities", "CrossRange", "GroundRange"],
+                values,
+            ),
+            "EarthRelativeWindV": (
+                ["Ambiguities", "CrossRange", "GroundRange"],
+                values,
+            ),
         },
         coords={
-            'Ambiguities': np.arange(4),
-            'CrossRange': np.arange(5),
-            'GroundRange': np.arange(4)
-        })
+            "Ambiguities": np.arange(4),
+            "CrossRange": np.arange(5),
+            "GroundRange": np.arange(4),
+        },
+    )
 
 
 def test_calculate_Euclidian_distance_to_neighbours(L2_small2D, lmout):
-    """Test the Euclidian distance cost function without centre cell"""
-    total_cost = spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
-        lmout.isel(GroundRange=1, CrossRange=1),
-        L2_small2D,
-        windcurrentratio=1,
-        include_centre=False,
-        Euclidian_method="standard",
+    """Test the Euclidian distance distance function without centre cell"""
+    total_distance = (
+        spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
+            lmout.isel(GroundRange=1, CrossRange=1),
+            L2_small2D,
+            windcurrentratio=1,
+            include_centre=False,
+            Euclidian_method="standard",
+        )
     )
     npt.assert_array_almost_equal(
-        total_cost, [67.88225100, 45.25483400, 22.62741700, 0.])
+        total_distance, [67.88225100, 45.25483400, 22.62741700, 0.0]
+    )
 
 
 def test_calculate_Euclidian_distance_to_neigbours_and_centre(L2_small2D, lmout):
-    """Test the Euclidian distance cost function with centre cell included"""
-    total_cost = spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
-        lmout.isel(GroundRange=1, CrossRange=1),
-        L2_small2D,
-        windcurrentratio=1,
-        include_centre=True,
-        Euclidian_method="standard",
+    """Test the Euclidian distance distance function with centre cell included"""
+    total_distance = (
+        spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
+            lmout.isel(GroundRange=1, CrossRange=1),
+            L2_small2D,
+            windcurrentratio=1,
+            include_centre=True,
+            Euclidian_method="standard",
+        )
     )
     npt.assert_array_almost_equal(
-        total_cost, [76.36753237, 50.91168825, 25.45584412,  0.])
+        total_distance, [76.36753237, 50.91168825, 25.45584412, 0.0]
+    )
 
 
 def test_calculate_squared_Euclidian_distance_to_neighbours(L2_small2D, lmout):
-    """Test the squared Euclidian distance cost function without centre cell"""
-    total_cost = spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
-        lmout.isel(GroundRange=1, CrossRange=1),
-        L2_small2D,
-        windcurrentratio=1,
-        include_centre=False,
-        Euclidian_method="squared",
+    """Test the squared Euclidian distance distance function without centre cell"""
+    total_distance = (
+        spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
+            lmout.isel(GroundRange=1, CrossRange=1),
+            L2_small2D,
+            windcurrentratio=1,
+            include_centre=False,
+            Euclidian_method="squared",
+        )
     )
-    assert (total_cost == [288., 128., 32., 0.]).all()
+    assert (total_distance == [288.0, 128.0, 32.0, 0.0]).all()
 
 
-def test_calculate_squared_Euclidian_distance_to_neighbours_and_centre(L2_small2D,
-                                                                       lmout):
-    """Test the squared Euclidian distance cost function with centre cell included"""
-    total_cost = spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
-        lmout.isel(GroundRange=1, CrossRange=1),
-        L2_small2D,
-        windcurrentratio=1,
-        include_centre=True,
-        Euclidian_method="squared",
+def test_calculate_squared_Euclidian_distance_to_neighbours_and_centre(
+    L2_small2D, lmout
+):
+    """Test the squared Euclidian distance distance function with centre cell included"""
+    total_distance = (
+        spatial_ambiguity_selection.calculate_Euclidian_distance_to_neighbours(
+            lmout.isel(GroundRange=1, CrossRange=1),
+            L2_small2D,
+            windcurrentratio=1,
+            include_centre=True,
+            Euclidian_method="squared",
+        )
     )
-    assert (total_cost == [324., 144., 36., 0.]).all()
+    assert (total_distance == [324.0, 144.0, 36.0, 0.0]).all()
 
 
-def cost(L2_sel, L2_neighbours, **cost_function_kwargs):
-    """Fake cost function for testing"""
+def distance(L2_sel, L2_neighbours, **distance_function_kwargs):
+    """Fake distance function for testing"""
     return np.array([3, 2, 0, 1])
 
 
 @pytest.mark.parametrize("i_x, i_y", [(0, 0), (3, 2), (3, 3), (4, 2), (4, 3)])
 def test_single_cell_ambiguity_selection(lmout, initial, i_x, i_y):
-    """Test the selection of ambiguity with the lowest cost"""
+    """Test the selection of ambiguity with the lowest distance"""
     selected_ambiguity = spatial_ambiguity_selection.single_cell_ambiguity_selection(
-        lmout, initial, i_x, i_y, cost, window=3
+        lmout, initial, i_x, i_y, distance, window=3
     )
     assert selected_ambiguity == 2
 
@@ -132,7 +144,7 @@ def test_single_cell_ambiguity_selection(lmout, initial, i_x, i_y):
 def test_solve_ambiguity_spatial_selection(lmout, initial):
     """Test the solve ambiguity function"""
     L2_solved = spatial_ambiguity_selection.solve_ambiguity_spatial_selection(
-        lmout, initial, cost, pass_number=1, window=3
+        lmout, initial, distance, pass_number=1, window=3
     )
     correct = np.full((5, 4), 2)
     assert (L2_solved.CurrentU.values == correct).all()
