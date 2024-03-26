00df35b7065ed22ae70ea0a1b598550a30b7913c
Jakub Michalski <jmichalski3009@gmail.com>
2024-03-26 10:06:47 +0000

    fix imports

--- seastar/retrieval/ambiguity_removal.py
+++ seastar/retrieval/ambiguity_removal.py
@@ -6,7 +6,9 @@ import xarray as xr
 from scipy.optimize import least_squares
 import seastar
 from seastar.utils.tools import dotdict
-from spatial_ambiguity_selection import solve_ambiguity_spatial_selection
+from seastar.retrieval.spatial_ambiguity_selection import (
+    solve_ambiguity_spatial_selection,
+)
 
 # import seastar.gmfs.doppler
 import pdb
@@ -45,32 +47,45 @@ def ambiguity_closest_to_truth(lmout, truth, windcurrentratio=10):
     """
 
     mytruth = xr.Dataset()
-    (mytruth['EarthRelativeWindU'], mytruth['EarthRelativeWindV']) = \
+    (mytruth["EarthRelativeWindU"], mytruth["EarthRelativeWindV"]) = (
         seastar.utils.tools.windSpeedDir2UV(
-            truth.EarthRelativeWindSpeed, truth.EarthRelativeWindDirection)
-    (mytruth['CurrentU'], mytruth['CurrentV']) = \
-        seastar.utils.tools.currentVelDir2UV(
-            truth.CurrentVelocity, truth.CurrentDirection)
-    mytruth['x'] = xr.concat(
-        [mytruth['EarthRelativeWindU'], mytruth['EarthRelativeWindV'],
-            mytruth['CurrentU'], mytruth['CurrentV']],
-        'x_variables'
+            truth.EarthRelativeWindSpeed, truth.EarthRelativeWindDirection
+        )
+    )
+    (mytruth["CurrentU"], mytruth["CurrentV"]) = seastar.utils.tools.currentVelDir2UV(
+        truth.CurrentVelocity, truth.CurrentDirection
+    )
+    mytruth["x"] = xr.concat(
+        [
+            mytruth["EarthRelativeWindU"],
+            mytruth["EarthRelativeWindV"],
+            mytruth["CurrentU"],
+            mytruth["CurrentV"],
+        ],
+        "x_variables",
     )
 
     err = mytruth - lmout  # keep only the 'x' variable
 
-    err['dist_x_reduce'] = xr.concat(
-        [err.x.sel(x_variables='u')**2 + err.x.sel(x_variables='v')**2,
-         windcurrentratio * (err.x.sel(x_variables='c_u')**2 + err.x.sel(x_variables='c_v')**2)],
-        'x_reduce'
+    err["dist_x_reduce"] = xr.concat(
+        [
+            err.x.sel(x_variables="u") ** 2 + err.x.sel(x_variables="v") ** 2,
+            windcurrentratio
+            * (err.x.sel(x_variables="c_u") ** 2 + err.x.sel(x_variables="c_v") ** 2),
+        ],
+        "x_reduce",
     )
-    err.coords['x_reduce'] = ['uv', 'c_uv']
+    err.coords["x_reduce"] = ["uv", "c_uv"]
 
-    ind_dict = dict({
-        'windcurrent': err.dist_x_reduce.sum(dim='x_reduce').argmin(dim='Ambiguities'),
-        'wind': err.dist_x_reduce.sel(x_reduce='uv').argmin(dim='Ambiguities'),
-        'current': err.dist_x_reduce.sel(x_reduce='c_uv').argmin(dim='Ambiguities'),
-    })
+    ind_dict = dict(
+        {
+            "windcurrent": err.dist_x_reduce.sum(dim="x_reduce").argmin(
+                dim="Ambiguities"
+            ),
+            "wind": err.dist_x_reduce.sel(x_reduce="uv").argmin(dim="Ambiguities"),
+            "current": err.dist_x_reduce.sel(x_reduce="c_uv").argmin(dim="Ambiguities"),
+        }
+    )
 
     return ind_dict
 
@@ -92,7 +107,7 @@ def ambiguity_sort_by_cost(lmout):
         ``lmout.isel(Ambiguities=index_cost)``
     """
     cost = xr.where(np.isnan(lmout.cost), np.inf, lmout.cost)
-    index = cost.argmin(dim='Ambiguities')
+    index = cost.argmin(dim="Ambiguities")
     return index
 
 
@@ -127,31 +142,35 @@ def solve_ambiguity(lmout, ambiguity):
     sol : ``xarray.Dataset``
         solution with ambiguities resolved
     """
-    if 'name' not in ambiguity:
+    if "name" not in ambiguity:
         raise Exception(
-            "'name' should be provided with value between 'sort_by_cost', 'closest_truth', or 'spatial_selection'")
+            "'name' should be provided with value between 'sort_by_cost', 'closest_truth', or 'spatial_selection'"
+        )
 
-    if ambiguity['name'] == 'sort_by_cost':
+    if ambiguity["name"] == "sort_by_cost":
         index = ambiguity_sort_by_cost(lmout)
         sol = lmout.isel(Ambiguities=index)
-    elif ambiguity['name'] == 'closest_truth':
-        if 'method' not in ambiguity:
-            ambiguity['method'] = 'windcurrent'
-        elif ambiguity['method'] not in ['windcurrent', 'wind', 'current']:
+    elif ambiguity["name"] == "closest_truth":
+        if "method" not in ambiguity:
+            ambiguity["method"] = "windcurrent"
+        elif ambiguity["method"] not in ["windcurrent", "wind", "current"]:
             raise Exception(
-                "ambiguity['method'] should be 'windcurrent', 'wind' or 'current'")
-        if 'windcurrentratio' not in ambiguity:
-            ambiguity['windcurrentratio'] = 10
-        elif not ambiguity['windcurrentratio'] > 0:
+                "ambiguity['method'] should be 'windcurrent', 'wind' or 'current'"
+            )
+        if "windcurrentratio" not in ambiguity:
+            ambiguity["windcurrentratio"] = 10
+        elif not ambiguity["windcurrentratio"] > 0:
             raise Exception("ambiguity.windcurrentratio should be positive")
-        if 'truth' not in ambiguity:
-            raise Exception("ambiguity['truth'] HAVE to be provided for closest_truth method with"
-                            "same size as lmout")
-        index_dict = ambiguity_closest_to_truth(lmout,
-                                                ambiguity['truth'],
-                                                windcurrentratio=ambiguity['windcurrentratio'])
-        sol = lmout.isel(Ambiguities=index_dict[ambiguity['method']])
-    elif ambiguity['name'] == 'spatial_selection':
+        if "truth" not in ambiguity:
+            raise Exception(
+                "ambiguity['truth'] HAVE to be provided for closest_truth method with"
+                "same size as lmout"
+            )
+        index_dict = ambiguity_closest_to_truth(
+            lmout, ambiguity["truth"], windcurrentratio=ambiguity["windcurrentratio"]
+        )
+        sol = lmout.isel(Ambiguities=index_dict[ambiguity["method"]])
+    elif ambiguity["name"] == "spatial_selection":
         if "cost_function" not in ambiguity:
             raise Exception(
                 "ambiguity['cost_function'] HAVE to be provided for spatial_selection method"
@@ -166,6 +185,7 @@ def solve_ambiguity(lmout, ambiguity):
         sol = solve_ambiguity_spatial_selection(lmout, **ambiguity)
     else:
         raise Exception(
-            "ambiguity['name'] should be 'sort_by_cost', 'closest_truth', or 'spatial_selection'")
+            "ambiguity['name'] should be 'sort_by_cost', 'closest_truth', or 'spatial_selection'"
+        )
 
     return sol
