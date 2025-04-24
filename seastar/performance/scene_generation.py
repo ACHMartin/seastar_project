02a14f60a251ef10c41e758ecee2906aba88435b
Adrien Martin <admartin@noc.ac.uk>
2025-04-24 17:56:23 +0200

    modif of typing for the output for compute_truth_level1

--- seastar/performance/scene_generation.py
+++ seastar/performance/scene_generation.py
@@ -468,7 +468,7 @@ def compute_truth_level1(
         inst: xr.Dataset, geo: xr.Dataset, gmf: dict, 
         write_nc: Optional[bool]=False, 
         main_path: Optional[str]='.'
-        ) -> xr.Dataset, xr.Dataset:
+        ) -> tuple[xr.Dataset, xr.Dataset]:
     '''
     Generate 'Truth' and 'Level1' datasets from instrumental 'inst' and
     environmental/geophysical conditions 'geo' with given 'gmf'
