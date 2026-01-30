63b96c1c3083ab88e090659773ed3e03c53e7cdf
David McCann <107252303+DavidMcCann-NOC@users.noreply.github.com>
2026-01-30 15:14:57 +0000

    Update level1.py
    Logging the phase sign convention used

--- seastar/oscar/level1.py
+++ seastar/oscar/level1.py
@@ -716,6 +716,7 @@ def apply_phase_sign_convention(ds):
     try:
         config = seastar.utils.readers.read_config_OSCAR('phase_sign_convention',info_dict={'version':version})
         sign_convention = int(config[flight_date])
+        logger.info(f"Applying phase sign convention of {sign_convention}")
     except KeyError:
         logger.error(f"Flight '{flight_date}' not found in config file. Setting default phase sign convention to 1")
         sign_convention = 1    
